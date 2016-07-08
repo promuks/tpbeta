b5e11ca567a32fac71d15003b76fd71659c12b1d
promuks <promuks@users.noreply.github.com>
2016-07-08 10:04:29 -0400

    Update addldapcert.sh

--- tomcat-sti/scripts/addldapcert.sh
+++ tomcat-sti/scripts/addldapcert.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 server_java_home=$(java -XshowSettings:properties -version 2> /tmp/file1.txt ; cat /tmp/file1.txt | grep java.home | tr -d ' ' | cut -d '=' -f2)
+echo ${server_java_home}
 ${server_java_home}/bin/keytool -import -storetype JKS -storepass changeit -alias ATPCOLDAP -file /usr/local/tomcat/certs/ldap.cert -noprompt -keystore ${server_java_home}/lib/security/cacerts
